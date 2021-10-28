da127a1e8ac6e71d9de1d8132b72df75610ad46e
Your Name <graceliu@gracelius-iMac.home>
2021-10-28 17:05:12 -0400

    edit tasks feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,39 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click","p",function(){
+  //putting methods on a new line increase readability
+  var text =$(this)
+  .text()
+  .trim();
+  //swaping out "p" with "textarea"
+  var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
+$(".list-group").on("blur","textarea",function(){
+  //get the textarea's current value/text 
+  var text = $(this)
+   .val()
+   .trim();
+  //get the parent ul's id attribute
+  var status =$(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-","");
+  //get the task's position in the list of other li elements
+  var index =$(this)
+  .closest(".list-group-item")
+  .index();
+  //recreate p element
+  var taskP=$("<p>")
+  .addClass("m-1")
+  .text(text);
+  //replace textarea with p element
+  $(this).replaceWith(taskP);
+});
 
 
 
