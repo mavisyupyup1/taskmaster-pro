9f2963306f483556eb11aa63b273ef040a8cc2b9
Grace Liu <graceliu@Graces-Air.home>
2021-10-31 09:23:27 -0400

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,8 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
-
-
+  //check due date
+  auditTask(taskLi);
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -138,6 +138,9 @@ $(".list-group").on("change","input[type='text']",function(){
 
   //replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //pass taks's<li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 $(".card .list-group").sortable({
   connectWith:$(".card .list-group"),
@@ -187,6 +190,29 @@ $(".card .list-group").sortable({
   }
 });
 
+var auditTask =function(taskEl){
+  //ensure element is getting to the function
+  console.log(taskEl);
+  //get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  //ensure it worked;
+  console.log(date);
+
+  //convert to moment object at 5:00pm
+  var time = moment(date,"L").set("hour",17);
+  //this should print out an object for the value of the the date variable but at 5pm that day 
+  console.log(time);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger")
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)){
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if(Math.abs(moment().diff(time,"days")) <=2){
+    $(taskEl).addClass("list-group-item-warning");
+  }
+}
 $("#modalDueDate").datepicker({
   minDate:1
 });
